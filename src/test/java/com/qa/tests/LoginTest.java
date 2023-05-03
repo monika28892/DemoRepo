fda021c5ef3c9ba90dc6ea5fbef288763a317af6
Monika <monika28892@gmail.com>
2023-05-03 16:41:04 -0600

    first git commit for my selenium code

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+//import org.junit.jupiter.api.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+
+}
